25795744b7df3d345b08a8d362676cd0f82bc34e
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-01-06 23:44:07 +0600

    Setup the project

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="text-5xl text-center text-red-300">
+      <button className="btn btn-primary">Add to cart</button>
+    </div>
+  );
+};
+
+export default Home;
