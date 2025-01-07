ada7d30ce20365e303add4893d6d438c9fb454a0
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-01-07 22:59:11 +0600

    Added testimonials

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import PopularMenu from "./PopularMenu/PopularMenu";
+import Testimonials from "./Testimonials/Testimonials";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Banner />
       <Category />
       {/* <PopularMenu /> */}
+      <Testimonials />
     </div>
   );
 };
