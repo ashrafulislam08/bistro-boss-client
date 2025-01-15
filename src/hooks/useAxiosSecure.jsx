238cd02432845d5b52c8d0a00c759280af8f3fae
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-01-15 23:15:12 +0600

    Added all hooks

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,11 +1,39 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000'
-})
+  baseURL: "http://localhost:5000",
+});
 const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { logOut } = useAuth();
 
-    return axiosSecure;
+  // request interceptor to add authorization header for every secure call to the ap
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("access-token");
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async (error) => {
+      const status = error.response.status;
+      if (status === 401 || status === 403) {
+        await logOut();
+        navigate("/login");
+      }
+      return Promise.reject(error);
+    }
+  );
+  return axiosSecure;
 };
 
 export default useAxiosSecure;
